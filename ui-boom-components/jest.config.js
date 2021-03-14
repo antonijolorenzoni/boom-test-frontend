5ad0505606ff705a754f6f68b0e3ecbb31f33dc0
Antonijo Lorenzoni <antonijolorenzoni@Antonijos-MacBook-Pro.local>
2021-03-14 16:59:18 +0100

    added drone

--- ui-boom-components/jest.config.js
+++ ui-boom-components/jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   roots: ['./src'],
   transform: { '^.+\\.(ts|tsx|js|jsx)?$': 'ts-jest' },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testResultsProcessor: 'jest-allure-reporter',
   globals: {
     'ts-jest': {
       tsconfig: {
